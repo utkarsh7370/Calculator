94907bd47a497e1fa9953ac1715cf2739eb41238
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2022-02-17 17:33:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+// console.log("welcome to my calculator");
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        // console.log(buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            // screenValue = "";
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    });
+}
